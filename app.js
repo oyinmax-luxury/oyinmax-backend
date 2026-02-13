a4d1070f509f7c66b555d1fbf25749a9a5dad4e9
Charles <adeakinyemi129@gmail.com>
2026-02-14 00:50:14 +0100

    tested auth

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
@@ -15,9 +17,19 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
+
+
 //health check route
 app.get('/api/health', (req, res) => {
     res.status(200).json({message: "Oyinmax luxury backend api is running..."});
 });
+
+app.use('/api/auth', authRoutes);
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+
 
 module.exports = {app};
